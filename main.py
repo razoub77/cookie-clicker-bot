7d2e2f51b11b32b3efc224d3dee3a9ef1bea261c
razoub77 <razoub77@gmail.com>
2025-12-12 14:29:22 +0700

    Add clicking and buying buildings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, time
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -23,3 +23,34 @@ except NoSuchElementException:
     pass
 
 big_cookie = driver.find_element(By.ID, "bigCookie")
+
+
+def clicking():
+    timeout = time() + 5
+    while time() < timeout:
+        big_cookie.click()
+
+
+def check_building_upgrades():
+    buildings = driver.find_elements(
+        By.CSS_SELECTOR, "#products .product.unlocked.enabled"
+    )
+
+    if len(buildings) > 0:
+        highest_building = buildings[0]
+        for building in buildings:
+            price = building.find_element(By.CLASS_NAME, "price").text
+            highest_price = highest_building.find_element(By.CLASS_NAME, "price").text
+
+            price = int(price.replace(",", ""))
+            highest_price = int(highest_price.replace(",", ""))
+
+            if price > highest_price:
+                highest_building = building
+
+        highest_building.click()
+
+
+while True:
+    clicking()
+    check_building_upgrades()
